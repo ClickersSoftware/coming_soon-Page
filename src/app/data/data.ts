53ca300df063ae785ca6fae7c2ac61b361ca59b2
Muhammadtawil <muhammad.tawil@hotmail.com>
2023-12-07 21:16:47 +0200

    first commit

---
+++ src/app/data/data.ts
@@ -0,0 +1,26 @@
+const data = {
+    sitename: "Comming Soon Template",
+    sitetagline: "Comming Soon Template Tagline",
+    sitelogo: "",
+    title: "Comming Soon",
+    description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
+    newsletterheading: "Stay tuned for our live updates!",
+    copyrightText: 'Copyright © 2023 | Design and Developed By Clickers',
+    socialIconsHeading: "Follow Us 📣",
+    socialIcons: [
+        {
+            icon: "facebook",
+            link: "https://www.facebook.com/ClickersSoftware?mibextid=LQQJ4d",
+        },
+        {
+            icon: "Instagram",
+            link: "https://www.instagram.com/clickers_software/?utm_source=qr",
+        },
+        {
+            icon: "Github",
+            link: "#",
+        },
+    ],
+}
+
+export default data;
